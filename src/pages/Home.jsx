773c1dafaebd472067fe522cab778b1b466326b6
Quang Truong <truongtnq@gmail.com>
2022-03-27 15:09:41 +0700

    add MUI

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from 'react';
+import ResponsiveAppBar from '../components/AppBar/Bar';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+const CardMUI = () => {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component='img'
+        height='140'
+        image='https://upload.wikimedia.org/wikipedia/commons/3/36/Hopetoun_falls.jpg'
+        alt='green iguana'
+      />
+      <CardContent>
+        <Typography gutterBottom variant='h5' component='div'>
+          Lizard
+        </Typography>
+        <Typography variant='body2' color='text.secondary'>
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size='small'>Share</Button>
+        <Button size='small'>Learn More</Button>
+      </CardActions>
+    </Card>
+  );
+};
 
 const Home = () => {
   return (
-    <div>Home page</div>
-  )
-}
+    <div>
+      <ResponsiveAppBar  />
+      
+    </div>
+  );
+};
 
-export default Home
+export default Home;
