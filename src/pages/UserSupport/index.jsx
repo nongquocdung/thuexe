3ae0884e553a0ef7faee0c8c34d2f1c3459bbf3f
SoiAhihi <62455700+SoiAhihi@users.noreply.github.com>
2022-04-04 21:14:07 +0700

    router update for user

---
+++ src/pages/UserSupport/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const UserSupportPage = () => {
+  return (
+    <div>UserSupportPage</div>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,25 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import ResponsiveAppBar from '../components/AppBar/Bar'
-import FooterCustom from '../components/FooterCustom/FooterCustom'
+import React from "react";
+import { Outlet } from "react-router-dom";
+import ResponsiveAppBar from "../components/AppBar/Bar";
+import FooterCustom from "../components/FooterCustom/FooterCustom";
+import { DashboardLayout } from "../components/dashboard-layout";
 
-const MainPage = () => {
-
+export const MainPage = () => {
   return (
-    <div className='main-page'>
-        <ResponsiveAppBar/>
-        <div className='page'>
-            <Outlet/> 
-        </div>
-        <FooterCustom/>
+    <div className="main-page">
+      <ResponsiveAppBar />
+      <div className="page">
+        <Outlet />
+      </div>
+      <FooterCustom />
     </div>
-  )
-}
+  );
+};
 
-export default MainPage
+export const UserPage = () => {
+  return (
+    <DashboardLayout>
+      <Outlet />
+    </DashboardLayout>
+  );
+};
