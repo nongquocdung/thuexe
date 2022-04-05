96ed80fc30a48c4e2d055775c5faa40215628853
Dung Nong <qd24052000@gmail.com>
2022-04-05 21:13:53 +0700

    Add contents for UserAnnouncements

--- src/pages/UserAnnouncements/index.jsx
+++ src/pages/UserAnnouncements/index.jsx
@@ -1,7 +1,63 @@
-import React from 'react'
+import { Stack, Box } from "@mui/material";
+import * as React from "react";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import Divider from "@mui/material/Divider";
+import ListItemText from "@mui/material/ListItemText";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import Avatar from "@mui/material/Avatar";
+import Typography from "@mui/material/Typography";
+
+const Notification = () => {
+  return <div>aaaa</div>;
+};
 
 export const UserAnnouncementsPage = () => {
+  const notifications = [
+    {
+      title: "Brunch this weekend?",
+      sender: "Ali Connors",
+      message: `- I'll be in your neighborhood doing errands this…`,
+    },
+    {
+      title: "Summer BBQ",
+      sender: "Scott, Alex, Jennifer",
+      message: `- Wish I could come, but I'm out of town this…`,
+    },
+    {
+      title: "Oui Oui",
+      sender: "Sandra Adams",
+      message: `- Do you have Paris recommendations? Have you ever…`,
+    },
+  ];
+  const isProp = false;
   return (
-    <div>UserAnnouncementsPage</div>
-  )
-}
+    <List sx={{ width: "100%", maxWidth: 540, bgcolor: "background.paper" }}>
+      {notifications.map((notification) => {
+        return (
+          <ListItem alignItems="flex-start">
+            <ListItemAvatar>
+              <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+            </ListItemAvatar>
+            <ListItemText
+              primary={notification.title}
+              secondary={
+                <React.Fragment>
+                  <Typography
+                    sx={{ display: "inline" }}
+                    component="span"
+                    variant="body2"
+                    color="text.primary"
+                  >
+                    {notification.sender}
+                  </Typography>
+                  {notification.message}
+                </React.Fragment>
+              }
+            />
+          </ListItem>
+        );
+      })}
+    </List>
+  );
+};
