252708ac97df5549c13b75be0cf04bd7cb4505ce
SoiAhihi <62455700+SoiAhihi@users.noreply.github.com>
2022-04-05 21:15:44 +0700

    lien he

---
+++ src/components/Card/MainCard.js
@@ -0,0 +1,59 @@
+import { forwardRef } from 'react';
+
+// material-ui
+import { useTheme } from '@mui/material/styles';
+import { Card, CardContent, CardHeader, Divider, Typography } from '@mui/material';
+
+// constant
+const headerSX = {
+    '& .MuiCardHeader-action': { mr: 0 }
+};
+
+// ==============================|| CUSTOM MAIN CARD ||============================== //
+
+export const MainCard = forwardRef(
+    (
+        {
+            border = true,
+            boxShadow,
+            children,
+            content = true,
+            contentClass = '',
+            contentSX = {},
+            darkTitle,
+            secondary,
+            shadow,
+            sx = {},
+            title,
+            ...others
+        },
+    ) => {
+        const style ={
+            border: border ? '1px solid' : 'none',
+        }
+
+        return (
+            <Card
+                {...others}
+                style = {style}
+            >
+                {!darkTitle && title && <CardHeader sx={headerSX} title={title} action={secondary} />}
+                {darkTitle && title && (
+                    <CardHeader sx={headerSX} title={<Typography variant="caption">{title}</Typography>} action={secondary} />
+                )}
+
+                {/* content & header divider */}
+                {title && <Divider />}
+
+                {/* card content */}
+                {content && (
+                    <CardContent sx={contentSX} className={contentClass}>
+                        {children}
+                    </CardContent>
+                )}
+                {!content && children}
+            </Card>
+        );
+    }
+);
+
--- src/pages/UserSupport/index.jsx
+++ src/pages/UserSupport/index.jsx
@@ -1,7 +1,26 @@
+import { Grid } from '@mui/material'
 import React from 'react'
+import { MainCard } from '../../components/Card/MainCard'
+import MuiTypography from '@mui/material/Typography';
+
 
 export const UserSupportPage = () => {
+  const style={
+    borderRadius:"12px",
+    border:"1px solid rgba(144, 202, 249, 0.46)",
+    margin :"20px 10px"
+  }
   return (
-    <div>UserSupportPage</div>
+    <div style={style}>
+      <MainCard title="Basic Typography" darkTitle = {true}>
+        <Grid item xs={12} sm={6}>
+          <Grid container direction="column" spacing={1}>
+            <Grid item>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. A delectus repellat nam cumque id aliquam nemo doloremque repellendus incidunt quo. Ducimus, doloribus explicabo est ab voluptatem modi quo debitis atque?
+            </Grid>
+          </Grid>
+        </Grid>
+      </MainCard>
+    </div>
   )
 }
